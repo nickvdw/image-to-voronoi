6693f8218a234752f657c6970f0a215bc88c125a
Johnnynater <jlith1997@gmail.com>
2020-02-26 21:25:32 +0100

    Refactored the edge detection code

--- src/scripts/delaunayBasedRendering/centroidsFromEdges.js
+++ src/scripts/delaunayBasedRendering/centroidsFromEdges.js
@@ -6,7 +6,7 @@ import * as d3Delaunay from "d3-delaunay";
 
 import { colourCentroidsByCoordinates } from "@/scripts/imageHandler";
 
-export const resultFromDelaunayCorners = (
+export const resultFromDelaunayEdges = (
   originalImageData,
   threshold,
   displayEdges,
@@ -15,6 +15,7 @@ export const resultFromDelaunayCorners = (
   croppedImageData,
   coordinateMargins
 ) => {
+  
   // Set the threshold for the number of corners to detect
   window.fastThreshold = threshold;
   window.tracking.Fast.THRESHOLD = window.fastThreshold;
@@ -31,20 +32,57 @@ export const resultFromDelaunayCorners = (
     imageData.height
   );
 
-  // Find the corners in the grayscaled image using FAST
-  const corners = window.tracking.Fast.findCorners(
-    gray,
+  // Grayscaling reduced the size of the pixel array to 1/4th, we need to reconstruct it
+  let grayImageData = [];
+  for (let i = 0; i < gray.length; i++) {
+    grayImageData.push(gray[i], gray[i], gray[i], 255);
+  }
+
+  // Run Sobel edge detection on the image
+  const edgePixels = window.tracking.Image.sobel(
+    grayImageData,
     imageData.width,
     imageData.height
   );
 
-  // Store the centroids based on the corners
+  // Normalise all the values, as some values are over 255
+  let max = 0;
+  for (let i = 0; i < edgePixels.length; i += 4) {
+    if (edgePixels[i] > max) {
+      max = edgePixels[i];
+    }
+  }
+
+  // Set x to be our iterative and normalise all values
+  let x = 0;
+  while (x < edgePixels.length) {
+    edgePixels[x] = 255 * (edgePixels[x] / max);
+    x += 4;
+  }
+
+  // Reset x and link each value to a coordinate under the array 'edges'
+  let edges = [];
+  x = 0;
+  for (let i = 0; i < imageData.height; i++) {
+    for (let j = 0; j < imageData.width; j++) {
+      edges.push({
+        x: j,
+        y: i,
+        colour: Math.round(edgePixels[x])
+      });
+      x += 4;
+    }
+  }
+
+  // Centroids are now being made under a certain threshold condition (value over ...)
   let centroids = [];
-  for (let i = 0; i < corners.length; i += 2) {
-    centroids.push({
-      x: corners[i],
-      y: corners[i + 1]
-    });
+  for (let i = 0; i < edges.length; i++) {
+    if (edges[i].colour > 50) {
+      centroids.push({
+        x: edges[i].x,
+        y: edges[i].y
+      });
+    }
   }
 
   // Set the initial configuration of the svg
