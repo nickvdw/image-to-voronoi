450c09f96470be50d845a8e07752fca3e5e5655a
Johnnynater <jlith1997@gmail.com>
2020-03-17 19:12:42 +0100

    Finished the about page and started with the help page

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,5 +1,49 @@
 <template>
-  <div class="about">
-    <h1>This is the about page</h1>
-  </div>
+  <v-container fluid>
+    <p class="display-1 text-center py-5">Voronoier was brought to you by these three wonderful developers</p>
+    <v-row dense justify="center">
+      <v-col
+        v-for="item in items"
+        :key="item.name"
+        align="center"
+        cols="3"
+      >
+        <v-card
+          outlined
+          width="350px"
+          
+        >
+        <v-img
+          :src="item.image"
+          class="align-end"
+          width="350px"
+          height="400px"
+        />
+          <v-card-title class="justify-center">{{ item.name }}</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+    <p class="subtitle-1 text-center py-5">
+      This application was made as part of a research paper on transforming images into Voronoi diagrams based on the <i>n</i>-nearest centroids. <br>
+      This was done for the course [2IMV10] Visual Computing Project at the University of Technology in Eindhoven. <br>
+      For those who are interested and want to indulge themselves into technical details, the paper regarding this application can be found here.<br><br>
+    <p class="subtitle-1 text-center">
+      Furthermore, we would like to thank our supervisor Michael Burch for providing us with the necessary feedback.
+    </p>
+  </v-container>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: null,
+        items: [
+          { name: 'John van Lith', image: require('../../public/img/john.png') },
+          { name: 'Nick van de Waterlaat', image: require('../../public/img/nick.png') },
+          { name: 'Jurriën van Winden', image: require('../../public/img/jurrien.png') },
+        ],
+      }
+    },
+  }
+</script>
--- src/views/HelpPage.vue
+++ src/views/HelpPage.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="help">
-    <h1>This is the help page</h1>
-  </div>
+  <v-container>
+    <v-navigation-drawer floating permanent>
+      <v-list>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-container>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: null,
+        items: [
+          { title: 'General information', icon: 'dashboard' },
+          { title: 'Importing an image', icon: 'question_answer' },
+          { title: 'Algorithm', icon: 'question_answer' },
+          { title: 'Centroid generation', icon: 'question_answer' },
+          { title: 'Pruning', icon: 'question_answer' },
+          { title: 'Display', icon: 'question_answer' },
+          { title: 'Editing the diagram', icon: 'question_answer' },
+        ],
+      }
+    },
+  }
+</script>
