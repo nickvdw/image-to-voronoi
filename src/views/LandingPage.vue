cb2dd21200eb92e5a19e72541539e3b9df6327a3
Sourqe <nickvdw@users.noreply.github.com>
2020-03-18 18:44:33 +0100

    Removed footer

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-carousel
+    hide-delimiter-background
+    show-arrows-on-hoverv-model="model"
+    height="100%"
+    style="height:100%"
+  >
+    <v-carousel-item style="height: 100%">
+      <v-sheet color="blue-grey darken-1" height="100%" tile>
+        <v-row class="fill-height" align="center" justify="center">
+          <div class="display-3">Slide</div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+    <v-carousel-item style="height: 100%">
+      <v-sheet color="blue-grey darken-1" height="100%" tile>
+        <v-row class="fill-height" align="center" justify="center">
+          <div class="display-3">Slide</div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+    <v-carousel-item style="height: 100%">
+      <v-sheet color="blue-grey darken-1" height="100%" tile>
+        <v-row class="fill-height" align="center" justify="center">
+          <div class="display-3">Slide</div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      model: 0
+    };
+  }
+};
+</script>
